9a3427d5b488930ade36eb1e39474587ae0af8c6
Jose Caballero <jcaballeroj@66gmail.com>
2023-12-02 04:40:11 -0500

    Creando tabla para mostrar el listado de propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    $resultado = $_GET['resultado']?? null;
+    $resultado = $_GET['resultado'] ?? null; //busca si llega algun parametro de validacion, igualdad al isset()
     require '../includes/funciones.php';
     //añadiendo la barra de navegacion con php por medios de funciones y templates
     incluirTemplate('header');
@@ -17,6 +17,34 @@
             <p class="alerta exito"> Anuncio Creado Correctamente</p>
         <?php  endif; ?>   
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <!-- Listando propiedades -->
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td> <img src="" alt="" class="imagen-tabla"> </td>
+                    <td>$120000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar </a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
     </main>
 
     <?php
