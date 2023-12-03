d88210516e60d61f074e1d5cfb1cd17564f848b9
Jose Caballero <jcaballeroj@66gmail.com>
2023-12-02 22:53:50 -0500

    Mostrando propiedades en paginas principales

--- anuncio.php
+++ anuncio.php
@@ -1,43 +1,63 @@
 <?php
+    //validando id
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if (!$id) {
+        header('location: /');
+    }
+
+    //importar la conexion
+    require 'includes/config/database.php';
+    $db = conectarDB();
+    //consultar
+    $query = "SELECT * FROM propiedades where id = $id";
+    //leer los resultados
+    $resultado = mysqli_query($db, $query);
+
+    if (!$resultado->num_rows) {
+        header('location: /');
+    }
+
+    $propiedad = mysqli_fetch_assoc($resultado);
+
+
     require 'includes/funciones.php';
     //añadiendo la barra de navegacion con php por medios de funciones y templates
     incluirTemplate('header');
-    
+
 ?>
 
-    <main class="contenedor seccion contenido-centrado">
-        <h1>Casa en Venta Frente al Bosque</h1>
-        
-        <picture>
-            <source srcset="build/img/destacada.webp" type="image/webp">
-            <source srcset="build/img/destacada.jpg" type="image/jpeg">
-            <img loading="lazy" src="build/img/destacada.jpg" alt="imagen de la propiedad">
-        </picture>
+<main class="contenedor seccion contenido-centrado">
+    <h1><?php echo $propiedad['titulo']?></h1>
+    <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']?>" alt="anuncio">
 
-        <div class="resumen-propiedad">
-            <p class="precio">$3,000,000</p>
-            <ul class="iconos-caracteristicas">
-                <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                    <p>3</p>
-                </li>
-                <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                    <p>3</p>
-                </li>
-                <li>
-                    <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
-                    <p>4</p>
-                </li>
-            </ul>
 
-            <p>Bienvenido a tu refugio perfecto frente al bosque, donde la serenidad y la naturaleza se entrelazan para crear un entorno idílico. Esta encantadora casa en venta ofrece una experiencia única, con sus amplias ventanas que capturan las impresionantes vistas del frondoso bosque que se extiende majestuosamente justo al otro lado de tu puerta. El diseño arquitectónico ha sido cuidadosamente concebido para integrar la belleza natural en cada rincón, permitiendo que la luz natural bañe cada habitación y creando una conexión armoniosa entre el interior y el exuberante entorno exterior.</p>
-            
-            <p>Con una ubicación privilegiada, esta casa no solo es un hogar, sino una invitación constante a explorar la maravilla del bosque circundante. Imagina despertar cada mañana con el suave murmullo de las hojas y la brisa fresca que fluye a través de tu hogar. Esta propiedad única ofrece un santuario tranquilo lejos del bullicio urbano, donde la privacidad se combina con la majestuosidad de la naturaleza. Este es más que un simple hogar; es tu conexión personal con la belleza natural, una experiencia de vida que cautivará tus sentidos y te recordará la verdadera magia de vivir en armonía con la naturaleza.</p>
-        </div>
-    </main>
+    <div class="resumen-propiedad">
+        <p class="precio">$<?php echo $propiedad['precio']?></p>
+        <ul class="iconos-caracteristicas">
+            <li>
+                <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                <p><?php echo $propiedad['wc']?></p>
+            </li>
+            <li>
+                <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                <p><?php echo $propiedad['estacionamiento']?></p>
+            </li>
+            <li>
+                <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitaciones">
+                <p><?php echo $propiedad['habitaciones']?></p>
+            </li>
+        </ul>
 
-    <?php
+        <p><?php echo $propiedad['descripcion']?></p>
+
+    </div>
+</main>
+
+<?php
+
+    mysqli_close($db);
     //añadiendo la barra de navegacion con php
     incluirTemplate('footer');
 ?>
